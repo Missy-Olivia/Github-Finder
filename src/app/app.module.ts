24011c0c1afffd6b65d17d4d2f46bbbaa689bb78
Ngabire Olivia Missy <ngamissia@gmail.com>
2020-11-10 01:31:44 +0200

    added directives, pipes and  ts files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing-page/landing/landing.component';
 import { ProfileComponent } from './profile/profile.component';
+import { UserDirective } from './user.directive';
+import { TimePipe } from './time.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    ProfileComponent
+    ProfileComponent,
+    UserDirective,
+    TimePipe
   ],
   imports: [
     BrowserModule,
